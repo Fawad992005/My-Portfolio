daf1e7660dc190232971a2c4537aac91ed4d38b7
Fawad Ahmed <fawaadahmed514@gmail.com>
2026-03-14 17:14:39 +0500

    resume change

--- src/components/Home/Home.component.tsx
+++ src/components/Home/Home.component.tsx
@@ -62,8 +62,9 @@ const Home = () => {
             </div>
             <div className="mt-6">
               <a
-                href="/Resume.pdf"
-                download="Resume.pdf"
+                href="/FrontEndResume.pdf"
+                download="FrontEndResume.pdf"
+                rel="noopener noreferrer"
                 className="px-6 py-2 bg-green-600 text-black font-semibold rounded-lg shadow hover:bg-green-700 transition"
               >
                 Download Resume
